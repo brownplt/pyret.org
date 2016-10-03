b8f6a3f84f15cd2cf94afdb02b9c8ef038060ecc
Joe Gibbs Politz <joe.politz@gmail.com>
2016-10-03 16:26:33 -0400

    deploy to Brown ftp

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#!/bin/bash
+
+SHORT_COMMIT=`echo $TRAVIS_COMMIT | cut -c1-7`
+VERSION=$SHORT_COMMIT
+make pages
+
+tar czf site.tgz site
+
+# NOTE(joe):
+#
+# The "*" in front of dele makes the copy continue if the command fails.  This
+# makes it so new branches don't need to have someone manually make a file to
+# delete (see the -Q option in `man curl`).
+
+curl -u $FTP_USER:$FTP_PASS \
+     -Q "*dele pyret.org/site.tgz" \
+     --ftp-create-dirs \
+     -T site.tgz ftp://ftp.cs.brown.edu/pyret.org/site.tgz
+
