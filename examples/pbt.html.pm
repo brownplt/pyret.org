a0b957591c8c882f6165bfa109cfe75f33bdba86
Shriram Krishnamurthi <75030+shriram@users.noreply.github.com>
2025-06-25 12:53:31 -0400

    new feature: PBT

---
+++ examples/pbt.html.pm
@@ -0,0 +1,10 @@
+#lang pollen
+
+◊p{As programs get more sophisticated, it is often not enough to write
+  unit tests; we need to test properties. Pyret gives you ◊a[#:href
+  "https://pyret.org/docs/latest/testing.html#%28part._.Unary_.Test_.Operators%29"]{primitives}
+  for expressing the idea of values satisfying predicates.}
+
+◊p{Pyret does not, however, provide data generators for property-based
+  testing, because writing those is usually instructive and also a
+  good way to learn about and explore biases in data.}
