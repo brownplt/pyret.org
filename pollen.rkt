c0c2dd80d570053fd32133fdf5eb06b564b3bbae
Shriram Krishnamurthi <75030+shriram@users.noreply.github.com>
2025-07-01 22:00:42 -0400

    useful new primitive!

--- pollen.rkt
+++ pollen.rkt
@@ -5,3 +5,10 @@
   (define (omitted-path? p) 
     (or (string-suffix? (path->string p) "site")
         (string-suffix? (path->string p) "typescript"))))
+
+(provide link/new-tab)
+
+(define (link/new-tab url . bodies)
+  `(a ([href ,url]
+       [target "_blank"])
+      ,@bodies))
