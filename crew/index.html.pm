33767057afbbeb95199cb5d9c388ad36c8bd28f2
Shriram Krishnamurthi <75030+shriram@users.noreply.github.com>
2025-07-02 09:25:06 -0400

    ported crew file over

---
+++ crew/index.html.pm
@@ -0,0 +1,49 @@
+#lang pollen
+
+◊div[#:class "container"]{
+  ◊div[#:class "row d-flex justify-content-center"]{
+      ◊div[#:class "col-md-6"]{
+
+◊h1{The Pyret Crew}
+
+  ◊p{ Pyret started its life at the PLT group at Brown University in
+      Providence, RI, USA, and the team is now spread across several
+      institutions. Pyret’s design and development draw on the group’s
+      experience in programming languages, education, web security,
+      user interfaces, and more. }
+
+  ◊p{ Ben Lerner and Joe Gibbs Politz are the main stewards of the
+      language, and build and maintain the compiler and associated
+      tools. Daniel Patterson and Dorai Sitaram have helped design and
+      build its feel and core features. }
+
+  ◊p{ The work of Danny Yoo and Emmanuel Schanzer on Whalesong and
+      WeScheme have been indispensible both for inspiration and for
+      re-use. }
+
+  ◊p{ Pyret has numerous contributors. In no particular order, Jake
+      Ellis, Frank Goodman, Sam Ainsworth, John Ericson, Sorawee
+      Porncharoenwase, Alex St Laurent, Philip Blair, Matthew
+      Kolosick, Jack Wrenn, Sarah Fischmann, Justin Pombrio, Cody
+      Mello, Eric Caruso, Stevie Strickland, Liam Elberty, Kathi
+      Fisler, Emmanuel Schanzer, Emma Youndtsmith, Jonah Kagan, Hilmar
+      Hoffmann, and many users on the ◊code{pyret-discuss} mailing
+      list and the Pyret Discord board have contributed ideas and
+      code. }
+
+  ◊p{ Numerous school teachers and college educators have used Pyret
+      in their courses, and we are grateful to them and their
+      students. }
+
+  ◊p{ Early on, the students of Brown’s CSCI 0190, CSCI 1729, and CSCI
+      1730, along with Swarthmore’s CS91, were press-ganged into
+      service aboard the Pyret ship, but took to the high seas with
+      grace. }
+
+  ◊p{ Shriram Krishnamurthi provides the budget for t-shirts and
+      stickers. }
+
+}
+
+}
+}
