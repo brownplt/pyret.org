a0b601d87444b5d0addfe2c90306c3d4ef7ddbe5
Shriram Krishnamurthi <75030+shriram@users.noreply.github.com>
2025-07-02 09:34:34 -0400

    switching to templated template

--- template.html.pp
+++ template.html.pp
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+◊(require (file "/Users/sk/Desktop/r/sk/website-brown-cs/find-relative-root.rkt"))
+◊(define resource-relative-path (here->relative-path here))
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -17,23 +19,28 @@
     <meta name="twitter:title" content="◊(select-from-metas "title" metas)" />
     <meta name="twitter:image" content="https://www.pyret.org/img/pyret-logo.png" />
     <meta name="viewport"    content="width=device-width, initial-scale=1.0">
-    <link rel="icon"      href="./img/pyret-icon.png">
+    <link rel="icon"      href="◊|resource-relative-path|/img/pyret-icon.png">
     <link rel="canonical" href="@|full-uri|">
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="//cdn.jsdelivr.net/font-hack/2.019/css/hack.min.css">
-    <link rel="stylesheet" type="text/css" href="./bootstrap-css/bootstrap-5.0.2-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="./css/pygments.css">
-    <link rel="stylesheet" type="text/css" href="./css/codemirror.css">
-    <link rel="stylesheet" type="text/css" href="./css/scribble.css">
-    <link rel="stylesheet" type="text/css" href="./css/custom.css">
-    <script src="./bootstrap-css/bootstrap-5.0.2-dist/js/bootstrap.min.js">
-    <script type="module" src="./node_modules/pyret-embed/dist/pyret.js"></script>
+    <link rel="stylesheet" type="text/css" href="◊|resource-relative-path|/bootstrap-css/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="◊|resource-relative-path|/css/pygments.css">
+    <link rel="stylesheet" type="text/css" href="◊|resource-relative-path|/css/codemirror.css">
+    <link rel="stylesheet" type="text/css" href="◊|resource-relative-path|/css/scribble.css">
+    <link rel="stylesheet" type="text/css" href="◊|resource-relative-path|/css/custom.css">
+    <script src="◊|resource-relative-path|/bootstrap-css/bootstrap-5.0.2-dist/js/bootstrap.min.js">
+    <script type="module" src="◊|resource-relative-path|/node_modules/pyret-embed/dist/pyret.js"></script>
+    <style>
+      .more-navbar-space li {
+        margin-left: 1.5rem;
+      }
+    </style>
   </head>
   <body>
-    <header class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <header class="navbar navbar-expand-lg navbar-light bg-light fixed-top more-navbar-space">
       <div class="container">
         <a href="./index.html" class="navbar-brand">
-          <img class="navbar-logo" src="./img/pyret-banner.png"></img>
+          <img class="navbar-logo" src="◊|resource-relative-path|/img/pyret-banner.png"></img>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -43,7 +50,9 @@
              id="navbarSupportedContent">
           <ul class="navbar-nav me-auto">
             <li class="nav-item"><a class="nav-link fancyhover-above" href="/docs/latest/">Documentation</a></li>
-            <li class="nav-item"><a class="nav-link fancyhover-above" href="./pyret-code/">Why Pyret?</a></li>
+            <li class="nav-item"><a class="nav-link fancyhover-above" href="◊|resource-relative-path|/pyret-code/">Why Pyret?</a></li>
+            <li class="nav-item"><a class="nav-link fancyhover-above" href="◊|resource-relative-path|/crew/">The Pyret Crew</a></li>
+            <li class="nav-item"><a class="nav-link fancyhover-above" href="◊|resource-relative-path|/discuss/">Scuttlebutt</a></li>
             <li class="nav-item"><a class="nav-link fancyhover-above" href="https://github.com/brownplt/pyret-lang">Source Code</a></li>
           </ul>
         </div>
