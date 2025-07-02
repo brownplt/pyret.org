51db8a95a1af8a8fe749dd4d9d19cea7d63cb291
Shriram Krishnamurthi <75030+shriram@users.noreply.github.com>
2025-07-02 09:26:19 -0400

    ported discuss page over

---
+++ discuss/index.html.pm
@@ -0,0 +1,39 @@
+#lang pollen
+
+◊div[#:class "container"]{
+  ◊div[#:class "row d-flex justify-content-center"]{
+      ◊div[#:class "col-md-6"]{
+
+◊h1{News and Discussion}
+
+ ◊h2{Announcements}
+
+  ◊p{ If you’re interested in hearing about major updates to Pyret
+  (including new versions with updated features or
+  backwards-incompatible changes), sign up for the very low-volume
+  ◊code{pyret-announce} mailing list at: }
+
+  ◊p{
+    ◊link/new-tab["https://groups.google.com/forum/#!forum/pyret-announce"]{
+      ◊pre{https://groups.google.com/forum/#!forum/pyret-announce}}}
+
+ ◊h2{Discussion}
+
+  ◊p{ We used to host discussion on the
+  ◊code{pyret-discuss} mailing list at: }
+
+  ◊p{
+    ◊link/new-tab["https://groups.google.com/forum/#!forum/pyret-discuss"]{
+      ◊pre{https://groups.google.com/forum/#!forum/pyret-discuss}}}
+
+  ◊p{ but discussion has largely shifted to Discord: }
+
+  ◊p[#:class "text-center"]{
+    ◊link/new-tab["https://discord.gg/7aFMB3b6Mt"]{
+      ◊pre{https://discord.gg/7aFMB3b6Mt}}}
+
+
+}
+
+}
+}
