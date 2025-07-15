f807099934f38ed59b5a86709463c5f999c12d37
Shriram Krishnamurthi <75030+shriram@users.noreply.github.com>
2025-07-15 09:59:53 -0400

    for loops

---
+++ examples/forloops.html.pm
@@ -0,0 +1,9 @@
+#lang pollen
+
+◊p{
+   Pyret has
+   ◊a[#:href "https://pyret.org/docs/latest/A_Tour_of_Pyret.html#%28part._.For_loops%29"]{
+   “for loops”}.
+   They are more useful and powerful
+   than those in most other programming languages.
+   }
