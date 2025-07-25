425810842f913bca19db0c25038b486d0893e9f4
Shriram Krishnamurthi <75030+shriram@users.noreply.github.com>
2025-07-25 08:46:53 -0400

    proper quotes

--- examples/spies.html.pm
+++ examples/spies.html.pm
@@ -2,11 +2,11 @@
 
 ◊p{
    In many languages, the only easy way to examine values during program execution is
-   by using print commands, leading to what is often called ``printf-debugging''.
+   by using print commands, leading to what is often called “printf-debugging”.
    Pyret recognizes the value of examining dynamic values and provides an elegant
    way of doing this:
    ◊link/new-tab["https://pyret.org/docs/latest/s_spies.html"]{spies}.
-   Unlike with a ``printf'', spying presents well-formatted output: you can trace back
+   Unlike with a “printf”, spying presents well-formatted output: you can trace back
    to the source, the output looks different to distinguish from other printed output,
    values in the display are clickable, etc.
 }
