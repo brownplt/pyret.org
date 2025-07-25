42b2637021292d61c2a97a35a67444db6600644d
Shriram Krishnamurthi <75030+shriram@users.noreply.github.com>
2025-07-25 08:51:17 -0400

    ◊link/new-tab better than ◊a

--- examples/forloops.html.pm
+++ examples/forloops.html.pm
@@ -2,8 +2,7 @@
 
 ◊p{
    Pyret has
-   ◊a[#:href "https://pyret.org/docs/latest/A_Tour_of_Pyret.html#%28part._.For_loops%29"]{
-   “for loops”}.
+   ◊link/new-tab["https://pyret.org/docs/latest/A_Tour_of_Pyret.html#%28part._.For_loops%29"]{“for loops”}.
    They are more useful and powerful
    than those in most other programming languages.
    }
