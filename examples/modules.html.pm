066af7afaccae70fb0c306ef2923a34c8a8eed61
Shriram Krishnamurthi <shriram@gmail.com>
2025-07-30 14:10:31 -0400

    reference new location of code (wrong branch?)

--- examples/modules.html.pm
+++ examples/modules.html.pm
@@ -4,6 +4,6 @@
    Pyret provides modules, with a rich set of features to
    ◊link/new-tab["https://pyret.org/docs/latest/modules.html"]{control what is exported}.
    For instance, here is a module
-   ◊link/new-tab["https://drive.google.com/file/d/1KIysiRxphUAKIAXm08xENzubR_kq-Nss/view?usp=sharing"]{stored in Google Drive}
+   ◊link/new-tab["https://raw.githubusercontent.com/brownplt/pyret.org/refs/heads/modules-not-working/examples/code/home-page-modules.arr"]{stored on GitHub}
    that provides some support for tic-tac-toe.
 }
